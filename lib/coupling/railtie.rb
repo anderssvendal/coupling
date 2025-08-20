0d64de12310d8663a3bd0db8943a41503f366865
Anders Svendal <anders.svendal@gmail.com>
2025-08-20 09:16:05 +0200

    clear manifest in dev

--- lib/coupling/railtie.rb
+++ lib/coupling/railtie.rb
@@ -3,6 +3,16 @@ require 'coupling/rack/app'
 
 module Coupling
   class Railtie < Rails::Railtie
+    initializer "coupling.clear" do |app|
+      next unless Rails.env.development?
+
+      ActiveSupport.on_load(:action_controller) do
+          ActionDispatch::Callbacks.before do
+            Coupling.clear_manifest
+          end
+        end
+    end
+
     initializer 'coupling.helper' do
       ActiveSupport.on_load(:action_view) do
         include Helper
